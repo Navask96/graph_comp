55c4a747f7694ee48a32081636488c55ea626323
Navask96 <s17397@sci.pdn.ac.lk>
2023-09-18 16:39:26 +0530

    Rechart library used

--- src/App.js
+++ src/App.js
@@ -1,83 +1,92 @@
-import React, { useState } from 'react';
-import CanvasJSReact from '@canvasjs/react-charts';
-import chartData from './data.json';
+import React, { useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  Label,
+} from "recharts";
+import data from "./data.json";
+import "./App.css"; // Import your CSS file for styling
 
-const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+const colors = ["#8884d8", "#82ca9d", "#ffc658"];
 
-const App = () => {
-  const [selectedDataSets, setSelectedDataSets] = useState(['data1']); // Default to 'data1'
-
-  const handleDataSetChange = (event) => {
-    const value = event.target.value;
-    if (selectedDataSets.includes(value)) {
-      setSelectedDataSets(selectedDataSets.filter((set) => set !== value));
-    } else {
-      setSelectedDataSets([...selectedDataSets, value]);
-    }
-  };
-
-  const filteredData = chartData.data.filter((point) =>
-    selectedDataSets.includes(point.set)
+export default function App() {
+  const [seriesVisibility, setSeriesVisibility] = useState(
+    data.map(() => true) // Initialize all series as visible
   );
 
-  const options = {
-    animationEnabled: true,
-    exportEnabled: true,
-    theme: "light2",
-    title: {
-      text: "Bounce Rate by Week of Year",
-    },
-    axisY: {
-      title: "Bounce Rate",
-      suffix: "%",
-    },
-    axisX: {
-      title: "Week of Year",
-      prefix: "W",
-      interval: 2,
-      crosshair: {
-        enabled: true,
-        snapToDataPoint: true,
-      },
-    },
-    toolTip: {
-      shared: true, // Enable shared tooltip for all data points with the same X value
-    },
-    data: selectedDataSets.map((set) => ({
-      type: "line",
-      name: set,
-      showInLegend: true,
-      toolTipContent: `{name} - Week {x}: {y}%, `,
-      dataPoints: filteredData.filter((point) => point.set === set),
-    })),
+  const toggleSeriesVisibility = (index) => {
+    const updatedVisibility = [...seriesVisibility];
+    updatedVisibility[index] = !updatedVisibility[index];
+    setSeriesVisibility(updatedVisibility);
   };
 
   return (
-    <div>
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            value="data1"
-            checked={selectedDataSets.includes('data1')}
-            onChange={handleDataSetChange}
-          />
-          Data Set 1
-        </label>
-        <label>
-          <input
-            type="checkbox"
-            value="data2"
-            checked={selectedDataSets.includes('data2')}
-            onChange={handleDataSetChange}
-          />
-          Data Set 2
-        </label>
-        {/* Add checkboxes for other data sets if needed */}
+    <div className="centered-container">
+      <h2>Line Chart with Distinct Values</h2>
+      <div className="legend">
+        {data.map((s, index) => (
+          <div key={s.name} className="legend-item">
+            <label>
+              <input
+                type="checkbox"
+                checked={seriesVisibility[index]}
+                onChange={() => toggleSeriesVisibility(index)}
+              />
+              {s.name}
+            </label>
+          </div>
+        ))}
       </div>
-      <CanvasJSChart options={options} />
+      <LineChart
+        width={700}
+        height={400}
+        margin={{ top: 30, right: 30, left: 30, bottom: 30 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          dataKey="category"
+          type="category"
+          allowDuplicatedCategory={false}
+          interval={0}
+          tickCount={data[0].data.length}
+          height={60}
+          tick={{ dy: 10 }}
+        >
+          <Label
+            value="Categories"
+            position="insideBottom"
+            offset={2} // Add a 2px gap below the X-axis label
+            style={{ textAnchor: "middle" }}
+          />
+        </XAxis>
+        <YAxis>
+          <Label
+            value="Values"
+            angle={-90}
+            position="insideLeft"
+            offset={-10} // Add a 10px gap to the left of the Y-axis label
+            style={{ textAnchor: "middle" }}
+          />
+        </YAxis>
+        <Tooltip />
+        <Legend />
+        {/* Always render the lines, but apply conditional opacity based on visibility */}
+        {data.map((s, index) => (
+          <Line
+            dataKey="value"
+            data={s.data}
+            name={s.name}
+            key={s.name}
+            stroke={colors[index]}
+            opacity={seriesVisibility[index] ? 1 : 0} // Set opacity to 0 when series is hidden
+          />
+        ))}
+      </LineChart>
     </div>
   );
-};
-
-export default App;
+}
